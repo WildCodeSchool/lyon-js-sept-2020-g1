6500480b03c0065516767834f503a28a1d118790
SaKara69 <sabrina.karakog@gmail.com>
2020-10-22 11:28:43 +0200

    mise en place des routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import AboutUs from './components/AboutUs';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/Contact' component={Contact} />
+          <Route path='/AboutUs' component={AboutUs} />
+        </Switch>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/Contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/AboutUs'>About us</Link>
+          </li>
+        </ul>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
