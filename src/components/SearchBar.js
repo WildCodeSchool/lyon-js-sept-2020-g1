f5f8f7fdc342be557c4f770a6b47a777662c6454
Jean Ducret <jean.dcrt@gmail.com>
2020-11-02 12:11:04 +0100

    update SearchBar with material-ui button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    backgroundColor: '#D97D0D',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#f2ab27',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '200px',
+      height: '60px',
+      fontSize: '18px',
+    },
+  },
+}));
 
 // SEARCH BAR COMPONENT MADE WITH HOOKS
 
 const Searchbar = () => {
+  const classes = useStyles();
+
   // Initializing future state for test (with Hooks useState)
   const [currentIngredient, setCurrentIngredient] = useState('');
-  //const [ingredientsList, setIngredientsList] = useState([]);
 
   // Handling when users writes in input -> the value is stored in the state
   const handleSearch = (inputValue) => setCurrentIngredient({ inputValue });
-
-  // Adding the current ingredient into the ingredients list
 
   const options = [
     { value: 'chocolate', label: 'Chocolate' },
@@ -38,6 +54,9 @@ const Searchbar = () => {
         onInputChange={handleSearch}
         value={currentIngredient}
       />
+      <Button variant="contained" className={classes.button}>
+        Let's Cook
+      </Button>
     </>
   );
 };
