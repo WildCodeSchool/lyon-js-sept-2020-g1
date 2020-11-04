1564978586b3f51a811515062deda43cabec45b6
SaKara69 <sabrina.karakog@gmail.com>
2020-11-04 09:39:19 +0100

    gestion conflits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import NavbarIngredients from './NavbarIngredients';
@@ -9,6 +9,10 @@ export default function Home() {
   // Initializing future state for test (with Hooks useState)
   const [currentIngredient, setCurrentIngredient] = useState('');
   const [ingredientsList, setIngredientsList] = useState([]);
+  // API KEY INITIALIZATION : the API key must be stored in .env file at the root of the project :
+  // REACT_APP_API_KEY = <Your API Key>
+
+  const apiKey = `${process.env.REACT_APP_API_KEY}`;
 
   // Stockage résultat d'API
   const [recipes, setRecipes] = useState([]);
@@ -25,16 +29,23 @@ export default function Home() {
     console.log(ingredientsList);
   };
 
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
+  // Autocomplete function : Fetching ingredients when users types in SearchBar
 
-  // API KEY INITIALIZATION : the API key must be stored in .env file at the root of the project :
-  // REACT_APP_API_KEY = <Your API Key>
-
-  const apiKey = `${process.env.REACT_APP_API_KEY}`;
+  useEffect(() => {
+    const apiURL = `https://api.spoonacular.com/food/ingredients/search?apiKey=${apiKey}&query=${currentIngredient}`;
+    axios
+      .get(apiURL)
+      .then((res) => res.data.results)
+      .then((data) => {
+        const options = data.map((ingredient) => ({
+          value: ingredient.id,
+          label: ingredient.name,
+        }));
+        setIngredientsList(options);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => console.log('Over'));
+  }, [currentIngredient]);
 
   // Fechting recipes from selected ingredients
 
@@ -75,7 +86,7 @@ export default function Home() {
             addIngredientToList={addIngredientToList}
             handleSearch={handleSearch}
             resultatsRecipes={resultatsRecipes}
-            options={options}
+            options={ingredientsList}
           />
         </div>
       </div>
