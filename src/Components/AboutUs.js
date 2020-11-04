e9aaa9e3b68214066b5275582b6b4196a1924b74
SaKara69 <sabrina.karakog@gmail.com>
2020-11-04 23:25:15 +0100

    ajout avatars et centrage

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,111 +1,55 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 200,
-    maxWidth: 400,
-  },
-  container: {
-    display: 'flex',
-  },
-  title: {
-    fontSize: 14,
-  },
-});
+import { Grid, Paper } from '@material-ui/core/';
 
 export default function AboutUs() {
-  const classes = useStyles();
-
   return (
-    <>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        spacing={5}
-      >
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root}>
-            <CardContent>
-              <img src="https://via.placeholder.com/200" alt="visage" />
-              <Typography variant="h5" component="h2">
-                Christian
-              </Typography>
-
-              <Typography variant="body2" component="p">
-                Positive MIND from Guadeloupe !!
-                <br />
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root}>
-            <CardContent>
-              <img src="https://via.placeholder.com/200" alt="visage" />
-              <Typography variant="h5" component="h2">
-                Maître Jean
-              </Typography>
-
-              <Typography variant="body2" component="p">
-                Que de fées penchées sur son berceau !
-                <br />
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root}>
-            <CardContent>
-              <img src="https://via.placeholder.com/200" alt="visage" />
-              <Typography variant="h5" component="h2">
-                Gilles
-              </Typography>
-
-              <Typography variant="body2" component="p">
-                Serial coder gentil et discret!
-                <br />
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root}>
-            <CardContent>
-              <img src="https://via.placeholder.com/200" alt="visage" />
-              <Typography variant="h5" component="h2">
-                Brieuc
-              </Typography>
-
-              <Typography variant="body2" component="p">
-                Codeur/Joueur/Chanteur!
-                <br />
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root}>
-            <CardContent>
-              <img src="https://via.placeholder.com/200" alt="visage" />
-              <Typography variant="h5" component="h2">
-                Sabrina
-              </Typography>
-
-              <Typography variant="body2" component="p">
-                OH my god...
-                <br />
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+    <Grid
+      container
+      display="flex"
+      flexflow="row wrap"
+      justify="space-evenly"
+      spacing={5}
+      style={{
+        textAlign: 'center',
+        height: '100vh',
+        padding: 30,
+      }}
+    >
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 10 }}>
+          <img src="/images/Avatars/Christian.png" alt="Christian's portrait" />
+          <h3 style={{ fontSize: 30 }}>Christian</h3>
+          <p style={{ fontSize: 20 }}>Positive Mind from Guadeloupe!</p>
+        </Paper>
       </Grid>
-    </>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 10 }}>
+          <img src="/images/Avatars/Jean.png" alt="Jean's portrait" />
+          <h3 style={{ fontSize: 30 }}>Jean</h3>
+          <p style={{ fontSize: 20 }}>Le Blond, le vrai!...</p>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 10 }}>
+          <img src="/images/Avatars/Brieuc.png" alt="Brieuc's portrait" />
+          <h3 style={{ fontSize: 30 }}>Brieuc</h3>
+          <p style={{ fontSize: 20 }}>Codeur, Joueur, Chanteur!!</p>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 10 }}>
+          <img src="/images/Avatars/Gilles.png" alt="Gilles's portrait" />
+          <h3 style={{ fontSize: 30 }}>Gilles</h3>
+          <p style={{ fontSize: 20 }}>La discrétion, mais efficace!</p>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 10 }}>
+          <img src="/images/Avatars/Sabrina.png" alt="Sabrina's portrait" />
+          <h3 style={{ fontSize: 30 }}>Sabrina</h3>
+          <p style={{ fontSize: 20 }}>Oh my god...</p>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
